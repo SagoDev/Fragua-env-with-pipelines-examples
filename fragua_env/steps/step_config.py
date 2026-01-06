19203d2ec80f12798716fc06b46728584db57956
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2026-01-06 13:04:56 -0300

    Create step_config.py

---
+++ fragua_env/steps/step_config.py
@@ -0,0 +1,22 @@
+"""Configuration for creation of FraguaStep."""
+
+from fragua.core.step_index import FraguaStepIndex
+from fragua_env.steps.extraction import EXTRACTION_STEPS
+from fragua_env.steps.loading import LOADING_STEPS
+from fragua_env.steps.utility import UTILITY_STEPS
+
+DICTS_LIST = [EXTRACTION_STEPS, LOADING_STEPS, UTILITY_STEPS]
+
+
+def create_step_index() -> FraguaStepIndex:
+    """Create fragua-sets step index."""
+    step_index = FraguaStepIndex()
+
+    for step_dict in DICTS_LIST:
+        for name, step in step_dict.items():
+            step_index.register(name=name, builder=step)
+
+    return step_index
+
+
+STEP_INDEX = create_step_index()
