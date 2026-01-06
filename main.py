5bdbfeb78346415bb392d14c58eecbe03f6c014c
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2026-01-06 15:38:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-"""Main file."""
+"""File Docstring."""
+
+from fragua_env.env_config import ENV
+from fragua_env.utils.enums import Pipelines
+
+
+def main():
+    """Main Docstring."""
+
+    # [Optional]: Enums to validate pipelines names
+    api_to_excel = Pipelines.API_TO_EXCEL.value
+    excel_to_csv = Pipelines.EXCEL_TO_CSV.value
+
+    # Execute pipelines
+    ENV.run(api_to_excel)
+    ENV.run(excel_to_csv)
+
+
+if __name__ == "__main__":
+    main()
