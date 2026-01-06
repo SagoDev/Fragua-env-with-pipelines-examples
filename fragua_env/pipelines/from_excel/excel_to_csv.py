80f310e0166595cec7027c0c8a6f5f7372e3ca92
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2026-01-06 15:36:56 -0300

    Update excel_to_csv.py

--- fragua_env/pipelines/from_excel/excel_to_csv.py
+++ fragua_env/pipelines/from_excel/excel_to_csv.py
@@ -1,17 +1,18 @@
 """Pipeline for extract data from an excel to csv file."""
 
-from pathlib import Path
 from fragua import FraguaPipeline
 
 from fragua_env.steps import STEP_INDEX
+from fragua_env.utils.helpers import get_project_root
 
 # -----------------------------------
 # Pipeline Example: excel to csv
 # -----------------------------------
 
 # Path created for example use
-BASE_DIR = Path(__file__).parent
+BASE_DIR = BASE_DIR = get_project_root()
 INPUT_FILE = BASE_DIR / "test_files" / "input_files" / "test_data.xlsx"
+OUTPUT_FILE = BASE_DIR / "test_files" / "output_files"
 
 # Create Empty pipeline
 excel_to_csv = FraguaPipeline("excel_to_csv")
@@ -28,7 +29,7 @@ extract_from_excel = (
 # Create second step: load to csv
 load_to_csv = (
     STEP_INDEX.get("load_to_csv")
-    .with_params(filename="excel_to_csv.csv")
+    .with_params(filename="excel_to_csv.csv", subdir=OUTPUT_FILE)
     .with_save_as("excel_to_csv_step_2")
     .with_use("excel_to_csv_step_1")
     .build()
