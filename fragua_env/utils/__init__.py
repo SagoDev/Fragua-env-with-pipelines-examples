f9f5b360168e4c33e8ec4797d93f80981176523d
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2026-01-06 17:16:05 -0300

    Update __init__.py

--- fragua_env/utils/__init__.py
+++ fragua_env/utils/__init__.py
@@ -1,5 +1,6 @@
 """Helper Module."""
 
 from .helpers import get_project_root
+from .enums import Pipelines, TransfomationFunction
 
-__all__ = ["get_project_root"]
+__all__ = ["get_project_root", "Pipelines", "TransfomationFunction"]
