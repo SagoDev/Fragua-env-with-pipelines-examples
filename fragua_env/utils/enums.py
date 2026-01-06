61a81408409bcac48a18893c6f52169568f8df15
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2026-01-06 15:36:24 -0300

    Create enums for pipelines

---
+++ fragua_env/utils/enums.py
@@ -0,0 +1,13 @@
+"""Enums File"""
+
+from enum import Enum
+
+
+# -------------------
+# Pipelines
+# -------------------
+class Pipelines(str, Enum):
+    """Pipelines"""
+
+    EXCEL_TO_CSV = "excel_to_csv"
+    API_TO_EXCEL = "api_to_excel"
