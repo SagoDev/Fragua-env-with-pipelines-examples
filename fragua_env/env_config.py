d6e8fbb0916259239751727dbce166398c14c749
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2026-01-06 14:24:43 -0300

    Environment config

--- fragua_env/env_config.py
+++ fragua_env/env_config.py
@@ -1,1 +1,22 @@
 """Fragua Environment Configuration."""
+
+import fragua as fg
+import fragua_sets as fs
+
+from .pipelines.pipelines_config import add_pipelines_set_to_registry
+
+# Create fragua environment
+
+env = fg.FraguaEnvironment("env-example")
+
+# Get environment registry
+registry = env.registry
+
+# Add pre-configurated sets from fragua-sets
+fs.add_sets_to_registry(fs.SETS_LIST, registry)
+
+# Add pipelines set to registry
+# The function was created in this project.
+add_pipelines_set_to_registry(registry)
+
+ENV = env
