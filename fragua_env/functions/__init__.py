62f142d6b1af71859382b406a82575b04805fc93
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2026-01-07 16:10:32 -0300

    Delete function module

--- fragua_env/functions/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Transform basic functions."""
-
-from .functions import TRANSFORMATION_FUNCTIONS
-from .functions_config import add_transformation_set_to_registry
-
-__all__ = ["TRANSFORMATION_FUNCTIONS", "add_transformation_set_to_registry"]
--- fragua_env/utils/enums.py
+++ fragua_env/utils/enums.py
@@ -11,17 +11,3 @@ class Pipelines(str, Enum):
 
     EXCEL_TO_CSV = "excel_to_csv"
     API_TO_EXCEL = "api_to_excel"
-
-
-# ------------------------
-# Transforamtion Functions
-# ------------------------
-
-
-class TransfomationFunction(str, Enum):
-    """Transformation functions."""
-
-    STRIP_WHITESPACE = "strip_whitespace"
-    FILL_MISSING_VALUES = "fill_missing_values"
-    CREATE_DERIVED_COLUMN = "create_derived_column"
-    CAPITALIZE_STRINGS_COLUMNS = "capitalize_string_columns"
