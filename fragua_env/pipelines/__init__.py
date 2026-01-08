1253d3a7145f47464ca740ce5a11c9bf1c4c71ae
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2026-01-08 19:02:32 -0300

    Update __init__.py

--- fragua_env/pipelines/__init__.py
+++ fragua_env/pipelines/__init__.py
@@ -1,5 +1,5 @@
 """Pipeline Functions Module."""
 
-from .pipelines_config import PIPELINES_FUNCTIONS, add_pipelines_set_to_registry
+from .from_api import *
 
-__all__ = ["PIPELINES_FUNCTIONS", "add_pipelines_set_to_registry"]
+from .from_excel import *
