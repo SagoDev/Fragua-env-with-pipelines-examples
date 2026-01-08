bf7d74508b1d7cfedac2b223ec98e6c9f5659e28
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2026-01-08 19:02:51 -0300

    Update excel_to_csv.py

--- fragua_env/pipelines/from_excel/excel_to_csv.py
+++ fragua_env/pipelines/from_excel/excel_to_csv.py
@@ -1,40 +1,35 @@
 """Pipeline for extract data from an excel to csv file."""
 
-from fragua import FraguaPipeline
+from typing import Any
+from fragua.utils.helpers import get_project_root
 
-from fragua_env.steps import STEP_INDEX
-from fragua_env.utils.helpers import get_project_root
-
-# -----------------------------------
-# Pipeline Example: excel to csv
-# -----------------------------------
 
 # Path created for example use
 BASE_DIR = BASE_DIR = get_project_root()
 INPUT_FILE = BASE_DIR / "test_files" / "input_files" / "test_data.xlsx"
 OUTPUT_FILE = BASE_DIR / "test_files" / "output_files"
 
-# Create Empty pipeline
-excel_to_csv = FraguaPipeline("excel_to_csv")
-
-# Create first step: extract from excel
-extract_from_excel = (
-    STEP_INDEX.get("extract_from_excel")
-    .with_params(path=INPUT_FILE)
-    .with_save_as("excel_to_csv_step_1")
-    .build()
-)
+# -----------------------------------
+# Declarative Pipeline Example: excel to csv
+# -----------------------------------
 
 
-# Create second step: load to csv
-load_to_csv = (
-    STEP_INDEX.get("load_to_csv")
-    .with_params(filename="excel_to_csv.csv", subdir=OUTPUT_FILE)
-    .with_save_as("excel_to_csv_step_2")
-    .with_use("excel_to_csv_step_1")
-    .build()
-)
-
-# Add steps to the pipeline
-step_list = [extract_from_excel, load_to_csv]
-excel_to_csv.add(step_list)
+# Create declarative pipeline
+EXCEL_TO_CSV_DEC_PIP: dict[str, Any] = {
+    "name": "excel_cleaning_pipeline",
+    "steps": [
+        {
+            "set": "extraction",
+            "function": "extract_from_excel",
+            "params": {"path": f"{INPUT_FILE}"},
+            "save_as": "raw_df",
+        },
+        {
+            "set": "loading",
+            "function": "load_to_csv",
+            "params": {"subdir": f"{OUTPUT_FILE}", "filename": "test_data.csv"},
+            "use": "raw_df",
+            "save_as": "final_df",
+        },
+    ],
+}
