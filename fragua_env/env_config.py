b2c40b43cd289b4d79dae484e555320f0b3ad91f
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2026-01-08 19:03:43 -0300

    Update env_config.py

--- fragua_env/env_config.py
+++ fragua_env/env_config.py
@@ -3,28 +3,11 @@
 import fragua as fg
 import fragua_sets as fs
 
-from fragua_env.functions.functions_config import add_transformation_set_to_registry
-
-from .pipelines.pipelines_config import add_pipelines_set_to_registry
-
 # Create fragua environment
-
 env = fg.FraguaEnvironment("env-example")
 
-# Get environment registry
-registry = env.registry
-
 # Add pre-configurated sets from fragua-sets
-fs.add_sets_to_registry(fs.SETS_LIST, registry)
-
-
-# The following functions were created in this project.
-
-# Add pipelines set to registry
-add_pipelines_set_to_registry(registry)
-
-# Add transformation set to registry
-add_transformation_set_to_registry(registry)
-
+pre_fg_sets = fs.FRAGUA_SETS
+env.add_sets(*pre_fg_sets)
 
 ENV = env
