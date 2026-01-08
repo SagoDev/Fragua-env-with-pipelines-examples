85f5abda5797d768e4e917a19d7768c8fbf5d17b
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2026-01-08 19:02:46 -0300

    Update __init__.py

--- fragua_env/pipelines/from_excel/__init__.py
+++ fragua_env/pipelines/from_excel/__init__.py
@@ -1,5 +1,6 @@
 """Pipelines for data extraction from an excel file to other destination type."""
 
-from .excel_to_csv import excel_to_csv
+from .excel_to_csv import EXCEL_TO_CSV_DEC_PIP
+from .excel_to_transformed_excel import EXCEL_TO_TFM_EXCEL_PIP
 
-__all__ = ["excel_to_csv"]
+__all__ = ["EXCEL_TO_CSV_DEC_PIP", "EXCEL_TO_TFM_EXCEL_PIP"]
