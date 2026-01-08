14a80ea963ef0d5ead30ad26518349873874f50b
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2026-01-08 19:13:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 """File Docstring."""
 
 from fragua_env.env_config import ENV
-from fragua_env.pipelines.from_excel import EXCEL_TO_CSV_DEC_PIP, EXCEL_TO_TFM_EXCEL_PIP
+from fragua_env.pipelines import (
+    EXCEL_TO_CSV,
+    EXCEL_TO_TFM_EXCEL,
+    API_TO_EXCEL,
+)
 
 
 def main():
     """Main Docstring."""
 
-    ENV.execute_pipeline(EXCEL_TO_CSV_DEC_PIP)
-    ENV.execute_pipeline(EXCEL_TO_TFM_EXCEL_PIP)
+    ENV.execute_pipeline(EXCEL_TO_CSV)
+    ENV.execute_pipeline(EXCEL_TO_TFM_EXCEL)
+    ENV.execute_pipeline(API_TO_EXCEL)
 
 
 if __name__ == "__main__":
