8f8105ae74cddeebdd191b3337862578a2a4e064
Santiago Lanz <133175709+SagoDev@users.noreply.github.com>
2026-01-09 10:05:27 -0300

    Update env_config.py

--- fragua_env/env_config.py
+++ fragua_env/env_config.py
@@ -2,6 +2,7 @@
 
 import fragua as fg
 import fragua_sets as fs
+from .pipelines import EXCEL_TO_CSV, EXCEL_TO_TFD_EXCEL, API_TO_EXCEL
 
 # Create fragua environment
 env = fg.FraguaEnvironment("env-example")
@@ -9,5 +10,9 @@ env = fg.FraguaEnvironment("env-example")
 # Add pre-configurated sets from fragua-sets
 pre_fg_sets = fs.FRAGUA_SETS
 env.add_sets(*pre_fg_sets)
+
+# Add pre-configurated declarative pipelines
+pipelines = [EXCEL_TO_TFD_EXCEL, EXCEL_TO_CSV, API_TO_EXCEL]
+env.add_pipelines(*pipelines)
 
 ENV = env
